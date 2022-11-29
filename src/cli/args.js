0c583815ccffd5a6ad7eeb5c47f3354795ec6525
masha.kul <m.kulikova@itransition.com>
2022-11-29 21:48:21 +0300

    feat: add parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  const PREFIX = '--';
+  const ars = process.argv.reduce((acc,value, index, array) =>
+    value.startsWith(PREFIX) ? [...acc, `${value.replace(PREFIX, '')} is ${array[index + 1]}`] : acc, []).join(', ');
+
+  console.log(ars);
 };
 
 parseArgs();
