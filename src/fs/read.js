dd5f9b65bbb9f321c989223f0e7e07619c346b1b
masha.kul <m.kulikova@itransition.com>
2022-11-29 21:18:10 +0300

    feat: add read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import { readFile } from 'fs/promises';
+import { exists, getAbsUrl } from './utils.js';
+import { ERROR_MSG, FILES_PATH } from './constants.js';
+
 const read = async () => {
-    // Write your code here 
+  const FILE_NAME = 'fileToRead.txt';
+  const BUFFER_ENCODING = 'utf8';
+  const fileUrl = getAbsUrl(`${FILES_PATH}/${FILE_NAME}`);
+  
+  if (await exists(fileUrl)) {
+    const content = await readFile(fileUrl, BUFFER_ENCODING);
+
+    console.warn(content);
+  } else {
+    throw Error(ERROR_MSG);
+  }
 };
 
 await read();
