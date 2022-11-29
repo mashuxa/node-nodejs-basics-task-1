2923493fd6936fc62cae0efd07f14f377f4f2199
masha.kul <m.kulikova@itransition.com>
2022-11-29 22:52:15 +0300

    feat: add calculate function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const FILE_PATH = './files/fileToCalculateHashFor.txt';
+  const ALGORITHM = 'sha256';
+  const ENCODING = 'hex';
+  const fileUrl = new URL(FILE_PATH, import.meta.url);
+
+  const content = await readFile(fileUrl);
+  const data = createHash(ALGORITHM).update(content);
+
+  console.warn(data.digest(ENCODING));
 };
 
 await calculateHash();
