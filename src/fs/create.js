c270136c4e827e51d80a25e54e8c845fc07067b8
masha.kul <m.kulikova@itransition.com>
2022-11-29 11:53:09 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { stat, writeFile } from 'fs/promises';
+
 const create = async () => {
-    // Write your code here 
+  const FILE_NAME = 'fresh.txt';
+  const FILE_PATH = 'files';
+  const CONTENT = 'I am fresh and young';
+  const ERROR_MSG = 'FS operation failed';
+
+  const url = new URL(`${FILE_PATH}/${FILE_NAME}`, import.meta.url);
+  const exists = async (path) => !!await stat(path).catch(() => false);
+
+  if (await exists(url)) {
+    throw Error(ERROR_MSG);
+  } else {
+    await writeFile(url, CONTENT);
+  }
 };
 
 await create();
