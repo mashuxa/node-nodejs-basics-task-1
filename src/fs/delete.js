3be6576f19429c26f8e7eecda75257f4a17fa48f
masha.kul <m.kulikova@itransition.com>
2022-11-29 20:54:32 +0300

    feat: add remove function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,16 @@
+import { rm } from 'fs/promises';
+import { exists, getAbsUrl } from './utils.js';
+import { FILES_PATH, ERROR_MSG } from './constants.js';
+
 const remove = async () => {
-    // Write your code here 
+  const FILE_NAME = 'fileToRemove.txt';
+  const fileUrl = getAbsUrl(`${FILES_PATH}/${FILE_NAME}`);
+
+  if (await exists(fileUrl)) {
+    await rm(fileUrl);
+  } else {
+    throw Error(ERROR_MSG);
+  }
 };
 
 await remove();
