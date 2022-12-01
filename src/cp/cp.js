d10630dc981d6512740f65466d3b967e68472860
masha.kul <m.kulikova@itransition.com>
2022-12-01 19:46:49 +0300

    feat: add spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,16 @@
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const SCRIPT_PATH = './files/script.js';
+  const scriptUrl = new URL(SCRIPT_PATH, import.meta.url);
+  const childProcess = fork(scriptUrl, args, { silent: true });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout)
+
+  childProcess.stdout.on('data', chunk => {
+     process.stdout.write(`Received from child process: ${chunk}`)
+  });
 };
 
 spawnChildProcess();
